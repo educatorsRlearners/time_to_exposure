ee4a1d816b1b84c926495ac60a4de799bcd61da6
educatorsRlearners <p.evansimpson@gmail.com>
2023-10-19 17:08:06 +0200

    Chore {main}: Conducted quality control

--- sql/events_live_3.6+.sql
+++ sql/events_live_3.6+.sql
@@ -14,8 +14,4 @@ select
 from events_live
 where (clientversion >= '3.6' or clientversion like '3.10.%')
     and gauserstartdate >= '2023-01-18'
-    and userid in (
-                '000ad428-f561-4586-b6bb-6587e6e39832'
-                , '00126038-0fd5-47a1-aced-39406c35fc0d'
-                , '00355778-afa1-4d23-bf22-5968bdd46509')
--- limit 400000
+    and userid in ('000ad428-f561-4586-b6bb-6587e6e39832')
